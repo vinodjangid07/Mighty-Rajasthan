7a746fbd8d16841dcd68ebe16742e5615c3d4c29
Vinod Jangid <infovinodjangid@gmail.com>
2023-08-30 11:58:02 +0530

    UI Update

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='Footer'>
+        <div className='FooterContainer'>
+          <div className="LinksContainer">
+            <h1>General</h1>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">About Us</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">Privacy Policy</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">Term & Conditions</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">Travel Policy</a>
+          </div>
+          <div className="LinksContainer">
+            <h1>Customer Service</h1>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">Contact Us</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">Travel History</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">FAQs</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">Another Policy</a>
+          </div>
+          <div className="LinksContainer">
+            <h1>My Journey</h1>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">About Us</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">Travel History</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">Travel Policy</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">History</a>
+          </div>
+          <div className="LinksContainer">
+            <h1>Socials</h1>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">Facebook</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">Instagram</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">E-Mail</a>
+            <a href="https://vinodjangid.me/Mighty-Rajasthan" className="FooterLinks">Whatsapp</a>
+          </div>
+        </div>
+        <div className='copyrightSection'>
+        <p>Momentous Holidays &#169; Copyright 2023 | All rights reserved</p>
+        </div>
+    </footer>
+  )
+}
